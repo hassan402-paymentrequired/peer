aab7b4a9a8dc943987891cae7fba8e7d77d7c176
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-10-19 13:32:36 +0100

    notif

--- app/Http/Controllers/Api/TestNotificationController.php
+++ app/Http/Controllers/Api/TestNotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\NotificationService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TestNotificationController extends Controller
 {
