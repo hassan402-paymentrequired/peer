4bfde7deaf466a425de7625f708cd8e3d190c26c
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-12-10 15:48:25 +0100

    added a scripti

---
+++ debug.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Jobs\FetchLeagues;
+use Illuminate\Contracts\Console\Kernel;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+$app->make(Kernel::class)->bootstrap();
+
+$id = (int)readline("Enter the league id: ");
+
+echo "Fetching leagues for league id: $id\n";
+
+dispatch(new FetchLeagues(id: $id));
+
+echo "Done\n";
