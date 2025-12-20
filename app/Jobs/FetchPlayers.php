8235f2964d5ff1a07e199ac4e25ca254e891f4a6
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-12-20 18:41:41 +0100

    job for test

--- app/Jobs/FetchPlayers.php
+++ app/Jobs/FetchPlayers.php
@@ -31,19 +31,21 @@ class FetchPlayers implements ShouldQueue
 
         while (true) {
             Log::info("Fetching players for league $leagueId, season $season, page $page...");
-            $response = \Illuminate\Support\Facades\Http::withHeaders([
-                'x-rapidapi-key' => $apiKey
-            ])->get($apiUrl, [
-                'league' => $leagueId,
-                'season' => $season,
-                'page' => $page
-            ]);
+            // $response = \Illuminate\Support\Facades\Http::withHeaders([
+            //     'x-rapidapi-key' => $apiKey
+            // ])->get($apiUrl, [
+            //     'league' => $leagueId,
+            //     'season' => $season,
+            //     'page' => $page
+            // ]);
+            $players = players_data(39, 2021);
 
-            $body = $response->json();
-            $players = $body['response'] ?? [];
-            $paging = $body['paging'] ?? ['current' => $page, 'total' => $page];
-            $currentPage = $paging['current'] ?? $page;
-            $totalPages = $paging['total'] ?? $page;
+
+            // $body = $response->json();
+            // $players = $body['response'] ?? [];
+            // $paging = $body['paging'] ?? ['current' => $page, 'total' => $page];
+            // $currentPage = $paging['current'] ?? $page;
+            // $totalPages = $paging['total'] ?? $page;
 
             Log::info("Total players fetched: " . count($players));
 
@@ -70,12 +72,58 @@ class FetchPlayers implements ShouldQueue
                 );
             }
 
-            if ($currentPage >= $totalPages) {
-                break;
-            }
-            $page++;
+            // if ($currentPage >= $totalPages) {
+            //     break;
+            // }
+            // $page++;
+            break;
         }
 
         Log::info('All players fetched and inserted/updated successfully.');
+    }
+
+    function call_api($endpoint, $params = [])
+    {
+
+        $parameters = '';
+        if (count($params) > 0) {
+            $parameters = '?' . http_build_query($params);
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://v3.football.api-sports.io/' . $endpoint . $parameters,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'x-rapidapi-key: YOUR_API_KEY_HERE'
+            ),
+        ));
+        $response = curl_exec($curl);
+        $response = json_decode($response);
+        curl_close($curl);
+        return $response;
+    }
+
+    function players_data($league, $season, $page = 1, $players_data = [])
+    {
+
+        $players = call_api('players', ['league' => $league, 'season' => $season, 'page' => $page]);
+        $players_data = array_merge($players_data, $players->response);
+
+        if ($players->paging->current < $players->paging->total) {
+
+            $page = $players->paging->current + 1;
+            if ($page % 2 == 1) {
+                sleep(1);
+            }
+            $players_data = players_data($league, $season, $page, $players_data);
+        }
+        return $players_data;
     }
 }
